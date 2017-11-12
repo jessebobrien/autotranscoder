e2ba2bc5d892c094ff6e6b66d795a13e6f7658db
Jesse OBrien <jesse.b.obrien@gmail.com>
2017-11-12 16:18:41 -0500

    handle existing transcoded files

--- find-and-transcode.sh
+++ find-and-transcode.sh
@@ -1,31 +1,19 @@
-#!/bin/bash
-
-# Usage:
-# Feed this script a 4k video file from a camera, and it will transcode it to dnxhr_hq 4k (for editing) in the same directory.
-#TODO - find and execute logic
-#TODO - handle command-line arguments (sourcedir, targetdir)
-#TODO - read current resolution from the input file to determine the ideal output resolution
-
-inputfile="$1"
-echo $inputfile
-# For now, transcode a file into dnxhr_hq using yuuv422p as a container.
-outputfile="$(basename ${inputfile} | cut -f 1 -d '.')_transcoded.MOV"
-rm $outputfile
-ffmpeg -i ${inputfile} -c:v dnxhd -profile:v dnxhr_hq -vf scale=4096x2160,fps=30000/1001,format=yuv422p -b:v 110M -c:a pcm_s16le ${outputfile}
-if [ $? -eq 0 ]
-then
-  resolution=$(ffmpeg -i $1 2>&1 | grep -oP 'Stream .*, \K[0-9]+x[0-9]+')
-  echo "Successfully transcoded to ${resolution} in yuv442p using dnxhr_hq. Output file is ${outputfile}"
-else
-  echo "Something went wrong with transcoding" >&2
-  exit 1
-fi
-
-# Let's make this an option, rather than the default.
-# If a file is elsewhere in the filesystem, it can totally just stay there.
-#TODO - make this a command-line argument.
-# mkdir -p originals
-# mv $inputfile originals
-echo "Finished."
-
-exit 0
+#!/bin/bash                                                                                                                    
+                                                                                
+sourcedir="$1"                                                                  
+transcodelist=$(find ${sourcedir} -iname "*.MP4")                               
+echo "Source directory: $sourcedir"                                             
+echo "List of files to be transcoded: $transcodelist"                           
+for sourcevideo in $transcodelist;                                              
+do                                                                              
+        shortname=$(basename $sourcevideo)                                      
+        targetname="${sourcevideo}_transcoded.MOV"                              
+        echo "*** Starting work on $(basename $sourcevideo)"                    
+        if [ ! -f ${targetname} ]; then                                         
+                echo "$shortname has not been transcoded yet."                  
+                $(ffmpeg -i ${sourcevideo} -c:v dnxhd -profile:v dnxhr_hq -vf scale=4096x2160,fps=30000/1001,format=yuv422p -b:v 110M -c:a pcm_s16le ${sourcevideo}_transcoded.MOV)
+                echo "done!"                                                    
+        else                                                                    
+                echo "$targetname already exists. Skipping."                    
+        fi                                                                      
+done
